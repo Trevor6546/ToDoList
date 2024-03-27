370ed50564c271487aef1b4ca24475b32e0d3e40
Injun Choi <147558213+injunc23@users.noreply.github.com>
2024-03-26 19:58:35 -0700

    checking off list function

--- app.py
+++ app.py
@@ -4,23 +4,11 @@ app = Flask(__name__)
 
 todos = []
 
-@app.route('/')
-def index():
-    return render_template('index.html', todos=todos)
 
-#allows the user to add a "todo" to their todo list
-@app.route('/add', methods=['POST'])
-def add_todo():
-    todo = request.form['todo']
-    todos.append({'task': todo, 'completed': False})  
-    return redirect(url_for('index'))
-
-#allows the user to delete a "todo" from their todo list
-@app.route('/delete/<int:index>')
-def delete_todo(index):
+@app.route('/complete/<int:index>')
+def complete_todo(index):
     if 0 <= index < len(todos):
-        del todos[index]  
+        todos[index]['completed'] = True  
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
